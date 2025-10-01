7963e046629619b22563a306e0d8b8d1beff937e
Matheus Gurther <matheusgurther@outlook.com>
2025-10-01 20:05:52 -0300

    Adicionando dependencias do allauth no settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tech',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
