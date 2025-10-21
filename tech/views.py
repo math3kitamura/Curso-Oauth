8177d8e2e4d897ec9d9924c042699b548b0c290d
Matheus <matheusgurther@outlook.com>
2025-10-20 21:34:50 -0300

    Adicionando confirmação de login para entrar em pagina members

--- tech/views.py
+++ tech/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
 
+@login_required
 def members(request):
     return render(request, 'members.html')
